2cf7f24022c24b563a039b28765cb61b1e40a790
visvasuthar <visvasuthar@gmail.com>
2022-04-14 14:34:33 -0700

    database connection

---
+++ Source/io_server/config.js
@@ -0,0 +1,9 @@
+const config = {
+    mysqlUser: "",
+    mysqlPassword: "",
+    mysqlHost: "",
+    mysqlDatabase: "",
+    mysqlPort: 
+}
+// Enter all the above details
+module.exports = config;
