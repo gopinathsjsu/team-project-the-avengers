d8cf5913f1acffe6fc18dcffe9fe298d889e0b5c
Faizali Mulla <faizalihidayatulla.mulla@sjsu.edu>
2022-04-16 22:27:53 -0700

    Application Server Connected To Database

--- Source/io_server/components/index/index.js
+++ Source/io_server/components/index/index.js
@@ -1,11 +1,20 @@
 const serverConfig = require('./../serverConfig/serverConfig');
 
 const express = require('express');
-const app = express();
+const app = express()
+
+var createUser = require('./../userSignup/createUser');
+var dbConnection = require('./../dbConfig/dbConnection');
 
 app.get('/', (req, res)=>{
-    res.send('Hey');
+    res.send('Index');
 });
+
+//#### ROUTES DEFINED STARTS ####
+
+app.use('/createUser', createUser);
+
+//#### ROUTES DEFINED ENDS ####
 
 app.listen(serverConfig.ioServer.port, ()=>{
     console.log('Server Running');
--- Source/io_server/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    mysqlUser: "",
-    mysqlPassword: "",
-    mysqlHost: "",
-    mysqlDatabase: "",
-    mysqlPort: 
-}
-// Enter all the above details
-module.exports = config;
