b2cc53553e7543f096f950c05a9c2354154349d8
visvasuthar <visvasuthar@gmail.com>
2022-04-16 21:30:08 -0700

    db updated

--- Source/database/hotelDB.sql
+++ Source/database/hotelDB.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `hotel` /*!40100 DEFAULT CHARACTER SET utf8mb4 CO
 USE `hotel`;
 -- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
 --
--- Host: localhost    Database: hotel
+-- Host: database-1.ca9ddimducmq.us-east-2.rds.amazonaws.com    Database: hotel
 -- ------------------------------------------------------
 -- Server version	8.0.28
 
@@ -16,6 +16,14 @@ USE `hotel`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
 
 --
 -- Table structure for table `amenities_table`
@@ -42,7 +50,7 @@ DROP TABLE IF EXISTS `bookings_table`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `bookings_table` (
   `bookings_id` int NOT NULL AUTO_INCREMENT,
-  `timestamp` datetime NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id` int NOT NULL,
   `hotel_id` int DEFAULT NULL,
   `room_id` int DEFAULT NULL,
@@ -56,11 +64,11 @@ CREATE TABLE `bookings_table` (
   KEY `hotel_id_idx` (`hotel_id`),
   KEY `room_id_idx` (`room_id`),
   KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `amenity_id` FOREIGN KEY (`amenity_id`) REFERENCES `amenities_table` (`amenity_id`),
-  CONSTRAINT `hotel_id` FOREIGN KEY (`hotel_id`) REFERENCES `hotel_table` (`hotel_id`),
-  CONSTRAINT `room_id` FOREIGN KEY (`room_id`) REFERENCES `rooms_table` (`room_id`),
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user_table` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `amenity_id_fk` FOREIGN KEY (`amenity_id`) REFERENCES `amenities_table` (`amenity_id`),
+  CONSTRAINT `hotel_id_fk` FOREIGN KEY (`hotel_id`) REFERENCES `hotel_table` (`hotel_id`),
+  CONSTRAINT `room_id_fk` FOREIGN KEY (`room_id`) REFERENCES `rooms_table` (`room_id`),
+  CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user_table` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -77,6 +85,22 @@ CREATE TABLE `country_table` (
   PRIMARY KEY (`country_id`),
   KEY `country_name_idx` (`country_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `dynamic_pricing`
+--
+
+DROP TABLE IF EXISTS `dynamic_pricing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dynamic_pricing` (
+  `dynamic_pricing_id` int NOT NULL AUTO_INCREMENT,
+  `rate_type` varchar(45) NOT NULL,
+  `rate_details` varchar(45) NOT NULL,
+  `rate_multiplier` float NOT NULL,
+  PRIMARY KEY (`dynamic_pricing_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,8 +121,10 @@ CREATE TABLE `hotel_table` (
   `costMultiple` float DEFAULT NULL,
   `listOfRooms` varchar(45) DEFAULT NULL,
   `mediaDirectory` varchar(45) DEFAULT NULL,
+   `room_id` int DEFAULT NULL,
   PRIMARY KEY (`hotel_id`),
   KEY `country_name_idx` (`country_name`),
+  KEY `room_id_idx` (`room_id`),
   CONSTRAINT `country_name` FOREIGN KEY (`country_name`) REFERENCES `country_table` (`country_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -135,12 +161,14 @@ CREATE TABLE `user_table` (
   `password` varchar(45) NOT NULL,
   `firstname` varchar(45) NOT NULL,
   `lastname` varchar(45) NOT NULL,
-  `create_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
   `phone` char(15) DEFAULT NULL,
   `dob` date DEFAULT NULL,
+  `user_type` varchar(45) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
