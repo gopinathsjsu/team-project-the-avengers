039b624a60ac6983452e65538c0fd70516770ee5
visvasuthar <visvasuthar@gmail.com>
2022-04-17 12:12:50 -0700

    search func

--- Source/io_server/searchHotels.js
+++ Source/io_server/searchHotels.js
@@ -1,9 +1,31 @@
+// return homepage
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+// search function    
+app.post('/search',function(req,res){
+    var str = {
+        stringPart:req.body.typeahead
+    }
+
+    db.query('SELECT city FROM hotel.hotel_table WHERE city LIKE "%'+str.stringPart+'%"',function(err, rows, fields) {
+        if (err) throw err;
+        var data=[];
+        for(i=0;i<rows.length;i++)
+        {
+            data.push(rows[i].songTitle);
+        }
+        res.send(JSON.stringify(data));
+    });
+});
+```
 const router = require("express").Router();
 const pool = require("../connection");
 
 router.post("/hotelId",(req,res)=>{
     
-    const hotelIdQuery = "select location from hotel;";
+    const hotelIdQuery = "select city from hotel_table;";
 
     pool.query(hotelIdQuery, [],(err,result)=>{
         console.log(result);
@@ -12,3 +34,4 @@ router.post("/hotelId",(req,res)=>{
 });
 
 module.exports = router;
+```
