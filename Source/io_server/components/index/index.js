02b4d479bbbbc1050832f2d96cbb3a504e9a6a18
Amy Phan <amyyyyphan99@gmail.com>
2022-04-21 20:29:30 -0700

    Edited server index.js

--- Source/io_server/components/index/index.js
+++ Source/io_server/components/index/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const express = require('express');
 const app = express()
+app.use(express.json());
 
 var userSignup = require('./../userSignup/router');
 global.dbPool = require('./../dbConfig/dbConnection');
@@ -11,8 +12,11 @@ global.dbPool = require('./../dbConfig/dbConnection');
 //     res.send('Index');
 // });
 
-app.use('/', express.static(path.join(__dirname, '..', '..', '..', 'web_ui', 'build')));
-
+// app.use('/', express.static(path.join(__dirname, '..', '..', '..', 'web_ui', 'build')));
+app.use(express.static(path.join(__dirname, '..', '..', '..', 'web_ui', 'build')));  // -Amy
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', '..', '..', 'web_ui', 'build', 'index.html'));
+});
 //#### ROUTES DEFINED STARTS ####
 
 app.use('/userSignup', userSignup);
