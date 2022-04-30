b68c4c2adcee4dad42d6df5c0ffe49d76093d0a5
Amy Phan <amyyyyphan99@gmail.com>
2022-04-29 18:27:27 -0700

    Admin file upload form

---
+++ Source/web_ui/src/pages/Admin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AdminFileUploadForm from '../components/AdminFileUploadForm';
+
+function Admin() {
+  return (
+    <AdminFileUploadForm />
+  )
+}
+
+export default Admin
