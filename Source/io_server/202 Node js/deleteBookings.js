559f4a8b2062b1b96fae01a456671e146d6ea69d
Adarsh <aadarshjn@gmail.com>
2022-05-02 00:14:18 -0700

    deleteBooking feature

--- Source/io_server/202 Node js/deleteBookings.js
+++ Source/io_server/202 Node js/deleteBookings.js
@@ -12,14 +12,77 @@ exports.deleteBookings = async (req,res)=>
   });
   var transaction_id=req.body.transaction_id;
   let ans={};
-  let result = await con.execute(` DELETE FROM main.Bookings where id =${transaction_id} `)
+  var possible=true;
+  var user_id;
+  var old_price;
+  let res1 = await con.execute(` SELECT start_date,user_id,price FROM main.Bookings where id =${transaction_id} `)
                   .then((res)=>{
-                    return "success";
+                    //return "success";
+                    var RES=res[0][0];
+                    var start_date= RES.start_date;
+                    user_id=RES.user_id;
+                    old_price=RES.price;
+                    var todayDate = new Date();
+                    todayDate.setHours(todayDate.getHours() - 7);
+
+                    console.log(start_date);
+                    console.log(todayDate);
+                    if(start_date<=todayDate)
+                    {
+                      possible=false;
+                      console.log("HERE");
+                    }
+
                     
                   })
                   .catch((err)=>{
                     console.log(err);
                     return "failure"
                   })
-                  res.json({"status":result});
+                  if(possible==false)
+                  {
+                    res.status(400);
+                    return res.json({"status":"Not possible"}) ;
+                    
+                  }
+                  
+ 
+  let result = await con.execute(` DELETE FROM main.Bookings where id =${transaction_id} `)
+                  .then((res)=>{
+                    
+                    
+                  })
+                  .catch((err)=>{
+                    console.log(err);
+                    
+                  })
+    //write code for updatting user point here
+    var user_points;
+    let result1 = await con.execute(` SELECT user_points from main.user_table where user_id='${user_id}' `)
+    .then((res)=>{
+                 
+      var RES=res[0][0];
+      user_points=RES.user_points;
+                    
+    })
+    .catch((err)=>{
+      console.log(err);
+      
+    })
+    user_points=user_points-(old_price)/10;
+    let result2=await con.execute(` UPDATE  main.user_table set user_points=${user_points} where user_id ='${user_id}' `)
+    .then((res)=>{
+                 
+      //var RES=res[0][0];
+      //user_points=RES.user_points;
+      return "Success"
+                    
+    })
+    .catch((err)=>{
+      console.log(err);
+      return "Failure";
+      
+    })
+      res.status(200);
+      res.json({"status":result2});
 }
