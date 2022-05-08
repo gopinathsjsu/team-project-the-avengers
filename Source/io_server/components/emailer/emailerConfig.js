0429e364e3832aec4a49d53f3e59f10dfa53021f
Faizali H. Mulla <faizalihidayatulla.mulla@sjsu.edu>
2022-05-08 15:30:49 -0700

    Emailer Config

---
+++ Source/io_server/components/emailer/emailerConfig.js
@@ -0,0 +1,11 @@
+const emailerConfig = {
+    host: "sg3plcpnl0015.prod.sin3.secureserver.net",
+    secure: false, 
+    auth: {
+      user: "faizali@justopensourceit.com",
+      pass: "E[Hxj1_5_Z-C",
+    },
+    tls:{
+        rejectUnauthorized: false
+    }
+  };
