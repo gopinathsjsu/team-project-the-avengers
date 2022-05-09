9e145bd69dfe1d9efed3f601c800347cc5f19fff
Visva <visvasuthar@gmail.com>
2022-05-09 08:56:28 -0700

    Revert "changes for admin auth"
    This reverts commit 8d6363e1be4ff23b3fd92926b4f1c9e58b45ed2f.

--- Source/io_server/components/admin/router.js
+++ Source/io_server/components/admin/router.js
@@ -3,7 +3,6 @@ var router = express.Router();
 const multer = require('multer');
 const path = require('path');
 
-var verifyToken = require('../userSignup/auth');
 var uploadFiles = require('./uploadFiles');
 
 
@@ -18,17 +17,9 @@ let storage = multer.diskStorage({
 });
 let upload = multer({storage: storage});
 
-router.post('/uploadFiles', upload.single('dataFile'), verifyToken, requiresAdmin, (req, res, next) =>{
+router.post('/uploadFiles', upload.single('dataFile'), (req, res) =>{
     uploadFiles(req, res, req.body.fileType);
 });
 
-function requiresAdmin(req, res, next){
-    if(req.user.admin !== true){
-        res.status(401).end();
-    }
-    else{
-        next();
-    }
-}
 
 module.exports = router;
